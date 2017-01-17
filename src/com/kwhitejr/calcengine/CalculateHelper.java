d6289546d495d0f80ef477e609b2e400788888ff
kwhitejr <kwhitejr.edu@gmail.com>
2017-01-17 12:54:07 -0800

    created override toString method on CalculateHelper for meaningful print

--- src/com/kwhitejr/calcengine/CalculateHelper.java
+++ src/com/kwhitejr/calcengine/CalculateHelper.java
@@ -4,6 +4,11 @@ package com.kwhitejr.calcengine;
  * Created by kevin on 1/17/17.
  */
 public class CalculateHelper {
+    private static final char ADD_SYMBOL = '+';
+    private static final char SUBTRACT_SYMBOL = '-';
+    private static final char MULTIPLY_SYMBOL = '*';
+    private static final char DIVIDE_SYMBOL = '/';
+
     MathCommand command;
     double dLeftVal;
     double dRightVal;
@@ -48,5 +53,35 @@ public class CalculateHelper {
         } else if (commandString.equalsIgnoreCase(MathCommand.Divide.toString())) {
             command = MathCommand.Divide;
         }
+    }
+
+    @Override
+    public String toString() {
+        char symbol = ' ';
+        switch(command) {
+            case Add:
+                symbol = ADD_SYMBOL;
+                break;
+            case Subtract:
+                symbol = SUBTRACT_SYMBOL;
+                break;
+            case Multiply:
+                symbol = MULTIPLY_SYMBOL;
+                break;
+            case Divide:
+                symbol = DIVIDE_SYMBOL;
+                break;
+        }
+
+        StringBuilder sb = new StringBuilder(20);
+        sb.append(dLeftVal);
+        sb.append(" ");
+        sb.append(symbol);
+        sb.append(" ");
+        sb.append(dRightVal);
+        sb.append(" = ");
+        sb.append(dResult);
+
+        return sb.toString();
     }
 }
